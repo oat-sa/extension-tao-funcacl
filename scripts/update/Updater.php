f3097833b6b607c6cc119acfa43964c04a499e2e
Mikhail Kamarouski <Komarouski@1pt.com>
2016-07-15 13:54:36 +0300

    safer way to revert changes

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -28,6 +28,6 @@ class Updater extends \common_ext_ExtensionUpdater
      * @return void
      */
     public function update($initialVersion) {
-        $this->skip('0','2.9.0');
+        $this->skip('0', '2.9.2');
 	}
 }
