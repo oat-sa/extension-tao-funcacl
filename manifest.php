1ff1be8ff7a157e88bb7c826355d28851620c6c5
Razhkou <yuryr@taotesting.com>
2020-03-16 15:32:32 +0300

    bump. upgrade to phpunit8

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return [
     'label' => 'Functionality ACL',
     'description' => 'Functionality Access Control Layer',
     'license' => 'GPL-2.0',
-    'version' => '5.4.0',
+    'version' => '5.5.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=27.0.0',
