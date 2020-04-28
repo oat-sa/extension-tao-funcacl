2c83b375f0eb8dd680e8a5dc4a4d0d3dc211caa0
Péter Halász <halaszpeterzsolt@gmail.com>
2020-04-28 18:25:41 +0200

    change response in actions

---
+++ test/unit/controller/AdminTest.php
@@ -0,0 +1,213 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\funcAcl\test\unit\controller;
+
+use oat\funcAcl\controller\Admin;
+use oat\funcAcl\models\ActionAccessService;
+use oat\funcAcl\models\ExtensionAccessService;
+use oat\funcAcl\models\ModuleAccessService;
+use oat\generis\test\MockObject;
+use oat\generis\test\TestCase;
+use oat\tao\model\service\ApplicationService;
+use Request;
+use tao_models_classes_Service;
+
+class AdminTest extends TestCase
+{
+    /** @var Admin|MockObject */
+    private $subject;
+
+    /** @var ApplicationService|MockObject */
+    private $applicationServiceMock;
+
+    /** @var ExtensionAccessService|MockObject */
+    private $extensionAccessServiceMock;
+
+    /** @var ModuleAccessService|MockObject */
+    private $moduleAccessServiceMock;
+
+    /** @var ActionAccessService|MockObject */
+    private $actionAccessServiceMock;
+
+    protected function setUp(): void
+    {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'xmlhttprequest';
+
+        // At the moment controllers cannot be tested properly so we partially mock the class under test
+        $this->subject = $this->getMock(
+            Admin::class,
+            ['getRequest', 'defaultData', 'prodLocker', 'returnJson']
+        );
+
+        $this->applicationServiceMock = $this->getMock(ApplicationService::class);
+        $this->extensionAccessServiceMock = $this->getMock(ExtensionAccessService::class, [], [], '', false);
+        $this->moduleAccessServiceMock = $this->getMock(ModuleAccessService::class, [], [], '', false);
+        $this->actionAccessServiceMock = $this->getMock(ActionAccessService::class, [], [], '', false);
+
+        $this->applicationServiceMock
+            ->expects($this->once())
+            ->method('isDebugMode')
+            ->willReturn(true);
+
+        $this->subject->setServiceLocator($this->getServiceLocatorMock([
+            ApplicationService::SERVICE_ID => $this->applicationServiceMock,
+        ]));
+
+        // AccessServices are not registered in the service manager, need to mock them in the global Service class
+        $instancesRef = new \ReflectionProperty(tao_models_classes_Service::class, 'instances');
+        $instancesRef->setAccessible(true);
+        $instancesRef->setValue(null, [
+            ExtensionAccessService::class => $this->extensionAccessServiceMock,
+            ModuleAccessService::class => $this->moduleAccessServiceMock,
+            ActionAccessService::class => $this->actionAccessServiceMock,
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = null;
+    }
+
+    public function testRemoveExtensionAccess(): void
+    {
+        $this->extensionAccessServiceMock
+            ->expects($this->once())
+            ->method('remove')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->removeExtensionAccess();
+    }
+
+    public function testAddExtensionAccess(): void
+    {
+        $this->extensionAccessServiceMock
+            ->expects($this->once())
+            ->method('add')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->addExtensionAccess();
+    }
+
+    public function testRemoveModuleAccess(): void
+    {
+        $this->moduleAccessServiceMock
+            ->expects($this->once())
+            ->method('remove')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->removeModuleAccess();
+    }
+
+    public function testAddModuleAccess(): void
+    {
+        $this->moduleAccessServiceMock
+            ->expects($this->once())
+            ->method('add')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->addModuleAccess();
+    }
+
+    public function testRemoveActionAccess(): void
+    {
+        $this->actionAccessServiceMock
+            ->expects($this->once())
+            ->method('remove')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->removeActionAccess();
+    }
+
+    public function testAddActionAccess(): void
+    {
+        $this->actionAccessServiceMock
+            ->expects($this->once())
+            ->method('add')
+            ->with('foo', 'bar');
+
+        $this->subject
+            ->method('getRequest')
+            ->willReturn($this->getRequestMock('foo', 'bar'));
+
+        $this->subject
+            ->expects($this->once())
+            ->method('returnJson')
+            ->with([
+                'uri' => 'bar',
+            ]);
+
+        $this->subject->addActionAccess();
+    }
+
+    /**
+     * @return Request|MockObject
+     */
+    private function getRequestMock(string $role, string $uri): Request
+    {
+        $requestMock = $this->getMock(Request::class);
+
+        $requestMock
+            ->method('getParameter')
+            ->withConsecutive(['role'], ['uri'])
+            ->willReturnOnConsecutiveCalls($role, $uri);
+
+        return $requestMock;
+    }
+}
