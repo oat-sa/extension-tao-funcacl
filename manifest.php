12bbcccc663a31985e5f93a2edcd9da0339524d6
bugalot <bugalot1985@gmail.com>
2020-06-24 18:21:58 +0200

    Bump version 6.0.0

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return [
     'label' => 'Functionality ACL',
     'description' => 'Functionality Access Control Layer',
     'license' => 'GPL-2.0',
-    'version' => '5.9.0',
+    'version' => '6.0.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=44.6.1',
