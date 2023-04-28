5bc19929025c2d1f701d50ec2ce4cb17799d5f26
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-04-28 10:47:38 +0200

    chore: Manually fix leftover PSR12 errors

--- models/classes/class.FuncAcl.php
+++ models/classes/class.FuncAcl.php
@@ -15,9 +15,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
+ * Copyright (c) 2013-2023 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT).
  */
+
+use oat\funcAcl\models\FuncAcl;
 
 /**
  * Proxy for the Acl Implementation
@@ -25,7 +26,9 @@
  * @access public
  * @author Joel Bout, <joel@taotesting.com>
  * @package tao
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName
  */
-class funcAcl_models_classes_FuncAcl extends \oat\funcAcl\models\FuncAcl
+class funcAcl_models_classes_FuncAcl extends FuncAcl
 {
 }
--- scripts/FuncAclTaoPreparePublicActions.php
+++ scripts/FuncAclTaoPreparePublicActions.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -15,10 +15,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg (under the project TAO & TAO2);
- *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung (under the project TAO-TRANSFER);
- *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
- *
+ * Copyright (c) 2002-2008 (original work) Public Research Centre Henri Tudor & University of Luxembourg
+ *                                         (under the project TAO & TAO2);
+ *               2008-2010 (update and modification) Deutsche Institut für Internationale Pädagogische Forschung
+ *                                                   (under the project TAO-TRANSFER);
+ *               2009-2012 (update and modification) Public Research Centre Henri Tudor
+ *                                                   (under the project TAO-SUSTAIN & TAO-DEV);
  */
 
 namespace oat\funcAcl\scripts;
@@ -32,7 +34,6 @@ use oat\funcAcl\models\FuncAclInitialisation;
  * @author Jérôme Bogaerts, <jerome@taotesting.com>
  * @author Joel Bout, <joel@taotesting.com>
  * @package tao
-
  */
 class FuncAclTaoPreparePublicActions extends \tao_scripts_Runner
 {
